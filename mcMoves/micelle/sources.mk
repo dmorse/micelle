ed7b1526bd11ff2f46a9e4f52425cb309c506263
Josh Mysona <myson001@umn.edu>
2019-03-18 18:10:21 -0500

    All McMoves compile

--- mcMoves/micelle/sources.mk
+++ mcMoves/micelle/sources.mk
@@ -1,9 +1,8 @@
 mcMd_modules_micelle_mcMoves_micelle_=\
-    mcMd/mcMoves/micelle/AggregatorMove.cpp \
-    mcMd/mcMoves/micelle/SingleMicelleHybridMove.cpp \
-    mcMd/mcMoves/micelle/SingleMicelleUmbrellaSamplingMove.cpp \
-    mcMd/mcMoves/micelle/ClusterIdentifierMC.cpp \
-    mcMd/mcMoves/micelle/RgUmbrellaMove.cpp
+    mcMd/modules/micelle/mcMoves/micelle/SingleMicelleHybridMove.cpp \
+    mcMd/modules/micelle/mcMoves/micelle/SingleMicelleUmbrellaSamplingMove.cpp \
+    mcMd/modules/micelle/mcMoves/micelle/ClusterIdentifierMC.cpp \
+    mcMd/modules/micelle/mcMoves/micelle/RgUmbrellaMove.cpp
 
 mcMd_modules_micelle_mcMoves_micelle_SRCS=\
      $(addprefix $(SRC_DIR)/, $(mcMd_modules_micelle_mcMoves_micelle_))
